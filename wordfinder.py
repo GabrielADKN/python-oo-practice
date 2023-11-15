93db59c094bc61c3915892efbdc948ae80ab6303
GabrielADKN <adoukonoubienheureux@gmail.com>
2023-11-15 10:33:22 -0500

    Add WordFinder class to find random words from dictionary

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,29 @@
 """Word Finder: finds random words from a dictionary."""
 
 
+import random
+
+
 class WordFinder:
-    ...
+    """Machine for finding random words from dictionary.
+
+    """
+    def __init__(self, path):
+        """Read dictionary and print # items read."""
+        dict_file = open(path)
+        self.words = self.parse(dict_file)
+        print(f"{len(self.words)} words read")
+    
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words."""
+        return [w.strip() for w in dict_file]
+    
+    def random(self):
+        """Return random word."""
+        return random.choice(self.words)
+    
+    
+wf = WordFinder("words.txt")
+print(wf.random())
+print(wf.random())
+print(wf.random())
